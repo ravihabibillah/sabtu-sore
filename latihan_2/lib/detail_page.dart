06fcf49d1829224c2bf387feac26e775d9bddaf7
ravihabibillah <ravihabibillah44@gmail.com>
2023-12-01 16:07:40 +0700

    add latihan 2

--- latihan_2/lib/detail_page.dart
+++ latihan_2/lib/detail_page.dart
@@ -8,102 +8,238 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Hero(
-              tag: detailWisata.thumbnail,
-              child: Image.asset(
-                detailWisata.thumbnail,
-                width: double.infinity,
-                height: 200,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Text(detailWisata.title,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
-                      )),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          Icon(Icons.date_range),
-                          SizedBox(
-                            height: 4,
-                          ),
-                          Text(detailWisata.openDays),
-                        ],
-                      ),
-                      // SizedBox(
-                      //   width: 16,
-                      // ),
-                      Column(
-                        children: [
-                          Icon(Icons.schedule),
-                          SizedBox(
-                            height: 4,
-                          ),
-                          Text(detailWisata.openHours),
-                        ],
-                      ),
-                      //  SizedBox(
-                      //   width: 16,
-                      // ),
-                      Column(
-                        children: [
-                          Icon(Icons.paid),
-                          SizedBox(
-                            height: 4,
-                          ),
-                          Text(detailWisata.price),
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Text(
-                    detailWisata.description,
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
+    return Scaffold(body: LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return DetailMobilePage(detailWisata: detailWisata);
+        } else {
+          return DetailWebPage(detailWisata: detailWisata);
+        }
+      },
+    ));
+  }
+}
+
+class DetailMobilePage extends StatelessWidget {
+  DetailMobilePage({super.key, required this.detailWisata});
+  DataWisata detailWisata;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Hero(
+            tag: detailWisata.thumbnail,
+            child: Image.asset(
+              detailWisata.thumbnail,
+              width: double.infinity,
               height: 200,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                shrinkWrap: true,
-                itemCount: detailWisata.imageUrls.length,
-                itemBuilder: (context, index) {
-                  var imageUrl = detailWisata.imageUrls[index];
-                  return Container(
-                    padding: const EdgeInsets.all(8),
-                    // decoration: BoxDecoration(
-                    //   borderRadius: BorderRadius.circular(100),
-                    // ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(16),
-                      child: Image.network(imageUrl),
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(detailWisata.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    )),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.date_range),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(detailWisata.openDays),
+                      ],
                     ),
-                  );
-                },
+                    // SizedBox(
+                    //   width: 16,
+                    // ),
+                    Column(
+                      children: [
+                        Icon(Icons.schedule),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(detailWisata.openHours),
+                      ],
+                    ),
+                    //  SizedBox(
+                    //   width: 16,
+                    // ),
+                    Column(
+                      children: [
+                        Icon(Icons.paid),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(detailWisata.price),
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  detailWisata.description,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 200,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemCount: detailWisata.imageUrls.length,
+              itemBuilder: (context, index) {
+                var imageUrl = detailWisata.imageUrls[index];
+                return Container(
+                  padding: const EdgeInsets.all(8),
+                  // decoration: BoxDecoration(
+                  //   borderRadius: BorderRadius.circular(100),
+                  // ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image.network(imageUrl),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DetailWebPage extends StatelessWidget {
+  DetailWebPage({super.key, required this.detailWisata});
+  DataWisata detailWisata;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(detailWisata.title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 36,
+                )),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // GAMBAR
+              Expanded(
+                child: Column(
+                  children: [
+                    Hero(
+                      tag: detailWisata.thumbnail,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.asset(
+                          detailWisata.thumbnail,
+                          width: double.infinity,
+                          height: 400,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 150,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemCount: detailWisata.imageUrls.length,
+                        itemBuilder: (context, index) {
+                          var imageUrl = detailWisata.imageUrls[index];
+                          return Container(
+                            padding: const EdgeInsets.all(8),
+                            // decoration: BoxDecoration(
+                            //   borderRadius: BorderRadius.circular(100),
+                            // ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(16),
+                              child: Image.network(imageUrl),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
-        ),
+              // DESKRIPSI
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Align(
+                            alignment: Alignment.topCenter,
+                            child: Text(
+                              detailWisata.location,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.date_range),
+                              SizedBox(
+                                width: 4,
+                              ),
+                              Text(detailWisata.openDays),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.schedule),
+                              SizedBox(
+                                width: 4,
+                              ),
+                              Text(detailWisata.openHours),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.paid),
+                              SizedBox(
+                                width: 4,
+                              ),
+                              Text(detailWisata.price),
+                            ],
+                          ),
+                          Text(detailWisata.description)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
--- latihan_2/lib/home_page.dart
+++ latihan_2/lib/home_page.dart
@@ -9,58 +9,140 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Daftar Wisata'),
+        title: const Text('Daftar Wisata'),
       ),
-      body: ListView.builder(
-          itemCount: listWisata.length,
-          itemBuilder: (context, index) {
-            var wisata = listWisata[index];
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return DetailPage(
-                    detailWisata: wisata,
-                  );
-                }));
-              },
-              child: Card(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Hero(
-                      tag: wisata.thumbnail,
-                      child: Image.asset(
-                        wisata.thumbnail,
-                        width: 150,
-                      ),
+      body: LayoutBuilder(builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return const ListViewWisata();
+        } else if (constraints.maxWidth < 1200) {
+          return const GridViewWisata(
+            crossAxisCount: 4,
+          );
+        } else {
+          return const GridViewWisata(
+            crossAxisCount: 6,
+          );
+        }
+      }),
+    );
+  }
+}
+
+class ListViewWisata extends StatelessWidget {
+  const ListViewWisata({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: listWisata.length,
+        itemBuilder: (context, index) {
+          var wisata = listWisata[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailPage(
+                  detailWisata: wisata,
+                );
+              }));
+            },
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Hero(
+                    tag: wisata.thumbnail,
+                    child: Image.asset(
+                      wisata.thumbnail,
+                      width: 150,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        // mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            wisata.title,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          wisata.title,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
                           ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            wisata.location,
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          wisata.location,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
-            );
-          }),
+            ),
+          );
+        });
+  }
+}
+
+class GridViewWisata extends StatelessWidget {
+  const GridViewWisata({super.key, required this.crossAxisCount});
+
+  final int crossAxisCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: crossAxisCount,
+      children: listWisata.map((wisata) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return DetailPage(
+                detailWisata: wisata,
+              );
+            }));
+          },
+          child: Card(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Hero(
+                    tag: wisata.thumbnail,
+                    child: Image.asset(
+                      wisata.thumbnail,
+                      width: double.infinity,
+                      // height: 150,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        wisata.title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        wisata.location,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      }).toList(),
     );
   }
 }
